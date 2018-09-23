e7cc68062721acd19682f707f2deb09ad1f079f7
sarrahhc <42785057+sarrahhc@users.noreply.github.com>
2018-09-23 18:49:53 -0400

    Challenge

---
+++ Assets/Scripts/MovingObjectScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovingObjectScript : MonoBehaviour
+{
+
+    public float speed = 2f;
+    private Rigidbody rb;
+    public Text countText;
+    public Text scoreText;
+    private int score;
+    private int count;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    public Rigidbody projectile;
+
+    void Update()
+    {
+        transform.Translate(-speed * Time.deltaTime, 0, speed * Time.deltaTime);
+    }
+
+}
